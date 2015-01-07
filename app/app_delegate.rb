020417397dca4d1e77367da704f4436a16e59687
Sergey Novikov <novikov359@gmail.com>
2015-01-08 01:35:33 +0300

    Added icon and notifications

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,9 @@ class AppDelegate
   attr_accessor :menu
 
   @@start_work_time = 0
+  @@work_time = 25
+  @@resting_time = 5
+  @@period_time = @@work_time + @@resting_time
 
   def applicationDidFinishLaunching(notification)
     @app_name = NSBundle.mainBundle.infoDictionary['CFBundleDisplayName']
@@ -11,7 +14,7 @@ class AppDelegate
     @menu_item = NSStatusBar.systemStatusBar.statusItemWithLength(NSVariableStatusItemLength).init
     @menu_item.setMenu(@menu)
     @menu_item.setHighlightMode(true)
-    @menu_item.setTitle(@app_name)
+    @menu_item.setImage(NSImage.imageNamed("icon"))
 
     @menu.addItem createMenuItem("Start Work", 'startWork')
     @menu.addItem createMenuItem("Quit", 'terminate:')
@@ -32,19 +35,35 @@ class AppDelegate
   end
 
   def working?
-    elapsed_time = elapsedTime + 5
-    (elapsed_time - ((elapsed_time / 30) * 30)) >= 5
+    elapsed_time = elapsedTime + @@resting_time
+    (elapsed_time - ((elapsed_time / @@period_time) * @@period_time)) >= @@resting_time
   end
 
-  def statusInWords
-    working? ? "Working" : "Resting"
+  def statusText(working)
+    working ? "Working" : "Resting"
+  end
+
+  def notificationText(working)
+    working ? "Get back to work" : "Time to rest"
+  end
+
+  def showNotification
+    notification = NSUserNotification.alloc.init
+    notification.title = "Pomidoro"
+    notification.informativeText = notificationText(working?)
+    notification.soundName = NSUserNotificationDefaultSoundName
+    NSUserNotificationCenter.defaultUserNotificationCenter.scheduleNotification(notification)
   end
 
   def checkWork
+    last_status_check ||= false
     while true
       if @@start_work_time > 0
-        @menu_item.setTitle(statusInWords)
+        @menu_item.setTitle statusText(working?)
+        showNotification if last_status_check != working?
+        last_status_check = working?
       end
+      sleep 1
     end
   end
 end
